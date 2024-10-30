bf346b32e9a09e17e79aff69f834492b158954f2
DanyaYulashev <crystalcat314@gmail.com>
2024-10-30 14:47:39 +0300

    Lab5 and 6

--- main5.c
+++ main5.c
@@ -4,84 +4,125 @@
 
 uint16_t dist = 0;
 
-
-ISR(INT7_vect) {
-    //если фронт сигнала на ECHO, то сбрасываем таймер
-    if((PINE & (1<<7)) != 0) {
-    TCNT1H = 0;
-    TCNT1L = 0;
+ISR(INT7_vect)
+{
+    // если фронт сигнала на ECHO, то сбрасываем таймер
+    if ((PINE & (1 << 7)) != 0)
+    {
+        TCNT1H = 0;
+        TCNT1L = 0;
     }
-    else { //если срез сигнала на ECHO, то забираем значение
-    dist = TCNT1L;
-    dist |= (TCNT1H << 8);
+    else
+    { // если срез сигнала на ECHO, то забираем значение
+        dist = TCNT1L;
+        dist |= (TCNT1H << 8);
     }
 }
 
 uint8_t find_non_zero(uint16_t d)
 {
-	uint8_t i = 1; uint16_t s = 10000;
-	while(d/s == 0)
-	{
-		i++;
-		s/=10;
-	}
-	return(i);
+    uint8_t i = 1;
+    uint16_t s = 10000;
+    while (d / s == 0)
+    {
+        i++;
+        s /= 10;
+    }
+    return (i);
 }
 
-uint8_t Digit (uint16_t d, uint8_t m)
+uint8_t Digit(uint16_t d, uint8_t m)
 {
-	uint8_t i = 5, a;
-	while(i)
-	{
-		a = d%10;
-		if (i-- == m) break;
-		d/=10;
-	}
-	return(a);
+    uint8_t i = 5, a;
+    while (i)
+    {
+        a = d % 10;
+        if (i-- == m)
+            break;
+        d /= 10;
+    }
+    return (a);
 }
 
 int main(void)
 {
     const uint8_t led[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+    const uint8_t close[] = {0x39, 0x38, 0x3F, 0x6D, 0x79};
+    const uint8_t far[] = {0x00, 0x00, 0x71, 0x77, 0x31};
+    uint8_t c = 0, shine = 0;
     DDRA = 0xFF;
-	DDRC = 0xFF;
+    DDRC = 0xFF;
     DDRE |= (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6);
     EIMSK |= (1 << INT7);
-    EICRB |= (1 << ISC71);
+    EICRB |= (1 << ISC70);
     TCCR1A = TCCR1A;
-    TCCR1B = (1 << CS12) | (1 << CS10)
+    TCCR1B = (1 << CS12) | (1 << CS10);
     TCCR3A = (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1) | (1 << WGM30);
     TCCR3B = (1 << WGM32) | (1 << CS31);
-    OCR3AH = 0; OCR3BH = 0; OCR3CH = 0;
+    OCR3AH = 0;
+    OCR3BH = 0;
+    OCR3CH = 0;
     sei();
-    while(1)
+    while (1)
     {
         PORTE |= (1 << 6);
-        _delay_us(15);
+        _delay_us(25);
         PORTE &= ~(1 << 6);
-        delay_ms(30);
-        
-        dist *= 1.57;
-        if(dist <= 10 || dist >= 100)
+        _delay_ms(200);
+
+        dist = ((float)(dist * 1.57));
+        shine = dist * 255 / 100;
+        if ((dist <= 10) || (dist >= 100))
         {
-            if(dist <= 10)
+            if (dist <= 10)
             {
-                dist = 10; OCR3AL = 255; OCR3BL = 255; OCR3CL = 255;
+                for (uint8_t i = 1; i <= 5; i++)
+                {
+                    PORTA |= (1 << i);
+                    PORTC = close[i-1];
+                    _delay_us(25);
+                    PORTA &= ~(1 << i);
+                }
+                OCR3AL = 255;
+                OCR3BL = 255;
+                OCR3CL = 255;
             }
-            if(dist >= 100)
+            if (dist >= 100)
             {
-                dist = 100; OCR3AL = 255; OCR3BL = 0; OCR3CL = 0;
+                for (uint8_t i = 1; i <= 5; i++)
+                {
+                    PORTA |= (1 << i);
+                    PORTC = far[i-1];
+                    _delay_us(25);
+                    PORTA &= ~(1 << i);
+                }
+                OCR3AL = 255;
+                OCR3BL = 0;
+                OCR3CL = 0;
             }
         }
         else
         {
-            OCR3AL = 255-dist*2.55; OCR3BL = dist*2.55; OCR3CL = dist*2.55;
+            OCR3AL = shine;
+            OCR3BL = 255 - shine;
+            OCR3CL = 255 - shine;
+            c = find_non_zero(dist);
+            for (uint8_t i = c; i <= 5; i++)
+            {
+                PORTA |= (1 << i);
+                PORTC = led[Digit(dist, i)];
+                _delay_us(25);
+                PORTA &= ~(1 << i);
+            }
+            for(uint8_t i = 1; i < c; i++)
+            {
+                PORTA |= (1 << i);
+                PORTC = 0x00;
+                _delay_us(25);
+                PORTA &= ~(1 << i);
+            }
         }
-        for(uint8_t i = 1; i <=5; i++)
-        {
-            PORTA |= (1 << i);
-            _delay_ms(1);
-            PORTC = led[digit(dist,i)];
-        }
+        
+        // _delay_ms(10);
     }
 }
---
+++ main6.c
@@ -0,0 +1,86 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DEV_ADDR 0b1100000 // адрес модуля по умолчанию
+#define W 0                // признак записи данных в матрицу
+#define R 1                // признак чтения данных из матрицы
+// регистр состояния диодов столбца
+#define COLUMN_DATA_REG 0x01
+// регистр обновления состояния диодов столбца
+#define UPDATE_COLUMN_REG 0x0C
+
+void twiStart();
+void twiStop();
+void twiWrite(uint8_t data);
+void matrDrawPict(uint8_t devAddr, uint8_t *dataBuf);
+void matrInit(uint8_t devAddr);
+
+int main(void)
+{
+    uint8_t pict[][8] = {{0xFF, 0b00011000, 0b00011000, 0xFF, 0x00, 0xFF, 0b10011001, 0b10011001},
+                         {0b00011000, 0b00011000, 0xFF, 0x00, 0xFF, 0b10011001, 0b10011001, 0b10011001},
+                         {0b00011000, 0xFF, 0x00, 0xFF, 0b10011001, 0b10011001, 0b10011001, 0x00},
+                         {0xFF, 0x00, 0xFF, 0b10011001, 0b10011001, 0b10011001, 0x00, 0xFF},
+                         {0x00, 0xFF, 0b10011001, 0b10011001, 0b10011001, 0x00, 0xFF, 0x01},
+                         {0xFF, 0b10011001, 0b10011001, 0b10011001, 0x00, 0xFF, 0x01, 0x01},
+                         {0b10011001, 0b10011001, 0b10011001, 0x00, 0xFF, 0x01, 0x01, 0x01},
+                         {0b10011001, 0b10011001, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00},
+                         {0b10011001, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF},
+                         {0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0x01},
+                         {0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0x01, 0x01},
+                         {0x01, 0x01, 0x01, 0x00, 0xFF, 0x01, 0x01, 0x01},
+                         {0x01, 0x01, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00},
+                         {0x01, 0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF},
+                         {0x00, 0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0b10000001},
+                         {0xFF, 0x01, 0x01, 0x01, 0x00, 0xFF, 0b10000001, 0b10000001},
+                         {0x01, 0x01, 0x01, 0x00, 0xFF, 0b10000001, 0b10000001, 0xFF}};
+    matrInit(DEV_ADDR);
+    while (1)
+    {
+        for(uint8_t i = 0; i < 17; i++)
+        {
+            matrDrawPict(DEV_ADDR, pict[i]);
+            _delay_ms(300);
+        }
+        _delay_ms(1000);
+    }
+}
+void twiStart(void)
+{ // состояние СТАРТ
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
+}
+void twiStop(void)
+{ // состояние СТОП
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+}
+void twiWrite(uint8_t data)
+{ // передача данных в матрицу
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
+}
+// передача изображения по столбцам на матрицу с МК
+void matrDrawPict(uint8_t devAddr, uint8_t *dataBuf)
+{
+    uint8_t i;
+    twiStart();
+    twiWrite((DEV_ADDR << 1) | W);
+    for (i = 0; i <= 7; i++)
+    {
+        // очередную часть рисунка кладём в очередной регистр
+        twiWrite(COLUMN_DATA_REG + i);
+        twiWrite(dataBuf[i]);
+    }
+    // чтобы рисунок был выведен, обновляем регистр 0x0C
+    twiWrite(UPDATE_COLUMN_REG);
+    twiWrite(0xFF);
+    twiStop();
+}
+void matrInit(uint8_t devAddr)
+{
+    TWBR = 32; // 200 кГц частота обмена
+    /* настройки по умолчанию (матрица 8х8, без
+    эквалайзера, 40мА тока) нам подходят, поэтому ничего
+    не меняем и не перенастраиваем */
+}
